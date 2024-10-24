df977e4a2a99141d57a086e7db870492f5079b85
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-10-24 13:59:56 +0530

    Create WeatherInformationApplication.java

---
+++ src/main/java/com/example/Weather_Info/_for_Pincode/WeatherInformationApplication.java
@@ -0,0 +1,12 @@
+package com.example.Weather_Info._for_Pincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherInformationApplication.class, args);
+	}
+}
