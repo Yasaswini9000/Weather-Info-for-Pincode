94174153219e3f303fd477419e7fea973e251baf
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-10-24 13:46:14 +0530

    Create WeatherController.java

---
+++ src/main/java/com/example/Weather_Info/_for_Pincode/controller/WeatherController.java
@@ -0,0 +1,42 @@
+package com.example.Weather_Info._for_Pincode.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Weather_Info._for_Pincode.model.WeatherInfo;
+import com.example.Weather_Info._for_Pincode.service.WeatherService;
+
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/weather")
+public class WeatherController {
+
+	@Autowired
+	private WeatherService weatherService;
+
+	@GetMapping("/getInfo")
+	public ResponseEntity<WeatherInfo> getWeatherInfo(@RequestParam Integer pincode,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+		WeatherInfo weather = null;
+
+		try {
+			weather = this.weatherService.getWeatherInfo(pincode, date);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<>(weather, HttpStatus.OK);
+	}
+}
